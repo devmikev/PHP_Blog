6b0f31b63b6399e72dce910a3e531cdc330cd739
Mike V <dev.mikev@gmail.com>
2018-12-13 11:53:53 -0500

    change password

--- config/config.php
+++ config/config.php
@@ -3,5 +3,5 @@
     define('ROOT_URL', 'http://localhost/myphp/ws8_mysql1/');
     define('DB_HOST', 'localhost');
     define('DB_USER', 'root');
-    define('DB_PASS', 'pitt12123');
+    define('DB_PASS', 'root123');
     define('DB_NAME', 'phpblog');
