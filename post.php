c837fdc3e4da79a272c30b8c9fa3e217cc915347
Mike V <dev.mikev@gmail.com>
2018-12-13 13:28:47 -0500

    complete insert, update and delete routes

--- post.php
+++ post.php
@@ -2,6 +2,23 @@
     require('config/config.php');
     require('config/db.php');
 
+    // Check for submit
+    if(isset($_POST['delete'])) {
+        // Get form data
+        $delete_id = mysqli_real_escape_string($conn, $_POST['delete_id']);
+
+        $query = "DELETE FROM posts WHERE id = {$delete_id}";
+        
+        // die($query);
+
+        if(mysqli_query($conn, $query)) {
+            header('Location: '.ROOT_URL.'');
+        } else {
+            echo 'Error: ' . mysqli_error($conn);
+        }
+    }
+
+    // get ID
     $id = mysqli_real_escape_string($conn, $_GET['id']);
 
     // Create Query
@@ -30,6 +47,11 @@
                 <small>Created on <?php echo $post['created_at']; ?>
                     <?php echo $post['author']; ?></small>
                     <p class="card-text"><?php echo $post['body']; ?></p>
+                    <form class="float-right" method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+                        <input type="hidden" name="delete_id" value="<?php echo $post['id']; ?>">
+                        <input type="submit" name="delete" value="Delete" class="btn btn-danger btn-sm">
+                    </form>
+                    <a class="btn btn-default" href="<?php echo ROOT_URL; ?>editpost.php?id=<?php echo $post['id']; ?>">Edit</a>
                     <a href="<?php echo ROOT_URL; ?>" class="btn btn-default">Go back</a>
             </div>
         </div>
